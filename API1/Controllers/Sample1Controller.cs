5a1bba4dfd33ec79a08b89924133a8b073202d59
Ahmad Noor <NourCS@hotmail.com>
2023-10-18 18:48:13 -0500

    init

---
+++ API1/Controllers/Sample1Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API1.Controllers
+{
+    public class Sample1Controller : Controller
+    {
+        [Route("GetAPI1")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Get from Sample API 1");
+        }
+    }
+}
---
+++ API2/Controllers/Sample2Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API2.Controllers
+{
+    public class Sample2Controller : Controller
+    {
+        [Route("PostAPI2")]
+        [HttpPost]
+        public IActionResult PostSample2([FromBody] string data)
+        {
+            return Ok($"Received data in Sample API 2: {data}");
+        }
+    }
+}
